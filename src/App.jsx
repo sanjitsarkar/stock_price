c924e32bdf7528b8d0865ef08d739158db71da0a
Sanjit Sarkar <ssanjit856@gmail.com>
2021-09-13 20:11:11 +0530

    added design and functionality

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [initialPrice, setInitialPrice] = useState();
+  const [numOfStocks, setNumOfStocks] = useState();
+  const [currentPrice, setCurrentPrice] = useState();
+  const [output, setOutput] = useState("");
+
+  const calculate = (e) => {
+    e.preventDefault();
+    if (initialPrice > currentPrice) {
+      var loss = (initialPrice - currentPrice) * numOfStocks;
+      var lossPercentage = (loss / initialPrice) * 100;
+
+      setOutput(
+        `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+      );
+    } else if (currentPrice > initialPrice) {
+      var profit = (currentPrice - initialPrice) * numOfStocks;
+      var profitPercentage = (profit / initialPrice) * 100;
+
+      setOutput(
+        `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+      );
+    } else {
+      setOutput(`No Profit No Loss`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Stock Profile & Loss Calculator</h1>
+
+      <form
+        onSubmit={(e) => {
+          calculate(e);
+        }}
+      >
+        <input
+          type="number"
+          value={initialPrice}
+          placeholder="Enter initial price"
+          onChange={(e) => {
+            setInitialPrice(e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Enter the number of stocks bought"
+          value={numOfStocks}
+          onChange={(e) => {
+            setNumOfStocks(e.target.value);
+          }}
+        />
+        <input
+          type="num"
+          value={currentPrice}
+          placeholder="Enter the current price of the stocks"
+          onChange={(e) => {
+            setCurrentPrice(e.target.value);
+          }}
+        />
+        <button type="submit">Calculate</button>
+        <p>{output}</p>
+      </form>
+    </div>
+  );
+}
+
+export default App;
